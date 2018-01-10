d76e57a7777df0a0eea87fb1cb355d0d2a529105
Mike Bell <mike@mercuna.com>
2018-01-10 22:35:27 +0000

    Use ulong arithmetic as it's faster.  Full range of test sizes.

--- PrimesCUDA/primetest.cu
+++ PrimesCUDA/primetest.cu
@@ -10,7 +10,7 @@
 typedef uint32_t uint;
 typedef uint64_t ulong;
 
-#define MAX_N_SIZE 128
+#define MAX_N_SIZE 64
 #define MAX_BLOCK_SIZE 32
 #define MAX_JOB_SIZE 2048
 
@@ -61,9 +61,9 @@ void fermat_test(const uint *M_in, const uint *Mi_in, const uint *R_in, uint *is
 						uint cy = 0;
 						for (int i = 0; i < N_Size - 1; ++i)
 						{
-							T[i] = R[i + 1] * R[0];
-							T[i] += cy;
-							cy = (T[i] < cy) + __umulhi(R[i + 1], R[0]);
+							ulong p = ulong(R[i + 1]) * ulong(R[0]) + cy;
+							T[i] = uint(p);
+							cy = uint(p >> 32);
 						}
 						T[N_Size - 1] = cy;
 					}
@@ -74,12 +74,11 @@ void fermat_test(const uint *M_in, const uint *Mi_in, const uint *R_in, uint *is
 						uint cy = 0;
 						for (int i = j; i < N_Size; ++i)
 						{
-							uint lp = R[i] * R[j - 1];
-							lp += cy;
-							cy = (lp < cy) + __umulhi(R[i], R[j - 1]);
-							lp += T[i + j - 2];
-							cy += lp < T[i + j - 2];
-							T[i + j - 2] = lp;
+							ulong p = ulong(R[i]) * ulong(R[j - 1]);
+							p += cy;
+							p += T[i + j - 2];
+							T[i + j - 2] = uint(p);
+							cy = uint(p >> 32);
 						}
 						T[N_Size + j - 2] = cy;
 					}
@@ -103,11 +102,10 @@ void fermat_test(const uint *M_in, const uint *Mi_in, const uint *R_in, uint *is
 					cy = 0;
 					for (int i = 0; i < N_Size * 2 - 2; ++i)
 					{
-						uint a = P[i + 1] + cy;
-						cy = (a < P[i + 1]);
+						ulong a = ulong(P[i + 1]) + cy;
 						a += T[i];
-						cy += (a < T[i]);
-						P[i + 1] = a;
+						P[i + 1] = uint(a);
+						cy = uint(a >> 32);
 					}
 					P[2 * N_Size - 1] += cy;
 				}
@@ -121,12 +119,10 @@ void fermat_test(const uint *M_in, const uint *Mi_in, const uint *R_in, uint *is
 					uint v = P[j] * mi;
 					for (int i = 0; i < N_Size; ++i)
 					{
-						uint lp = M[i] * v;
-						lp += cy;
-						cy = (lp < cy) + __umulhi(M[i], v);
-						lp += P[i + j];
-						cy += lp < P[i + j];
-						P[i + j] = lp;
+						ulong p = ulong(M[i]) * ulong(v) + cy;
+						p += P[i + j];
+						P[i + j] = uint(p);
+						cy = uint(p >> 32);
 					}
 					R[j] = cy;
 				}
@@ -135,11 +131,10 @@ void fermat_test(const uint *M_in, const uint *Mi_in, const uint *R_in, uint *is
 					uint cy = 0;
 					for (int i = 0; i < N_Size; ++i)
 					{
-						uint a = R[i] + cy;
-						cy = (a < R[i]);
+						ulong a = ulong(R[i]) + cy;
 						a += P[i + N_Size];
-						cy += (a < P[i + N_Size]);
-						R[i] = a;
+						R[i] = uint(a);
+						cy = uint(a >> 32);
 					}
 
 					if (cy != 0)
@@ -213,12 +208,10 @@ void fermat_test(const uint *M_in, const uint *Mi_in, const uint *R_in, uint *is
 			uint v = T[j] * mi;
 			for (int i = 0; i < N_Size; ++i)
 			{
-				uint lp = M[i] * v;
-				lp += cy;
-				cy = (lp < cy) + __umulhi(M[i], v);
-				lp += T[i + j];
-				cy += lp < T[i + j];
-				T[i + j] = lp;
+				ulong p = ulong(M[i]) * ulong(v) + cy;
+				p += T[i + j];
+				T[i + j] = uint(p);
+				cy = uint(p >> 32);
 			}
 			R[j] = cy;
 		}
@@ -227,11 +220,10 @@ void fermat_test(const uint *M_in, const uint *Mi_in, const uint *R_in, uint *is
 			uint cy = 0;
 			for (int i = 0; i < N_Size; ++i)
 			{
-				uint a = R[i] + cy;
-				cy = (a < R[i]);
+				ulong a = ulong(R[i]) + cy;
 				a += T[i + N_Size];
-				cy += (a < T[i + N_Size]);
-				R[i] = a;
+				R[i] = uint(a);
+				cy = uint(a >> 32);
 			}
 
 			if (cy != 0)
@@ -403,6 +395,12 @@ void primeTest(PrimeTestCxt* cxt, int N_Size, int listSize, const uint* M, uint*
 {
 	cudaError_t cudaStatus;
 
+	if (N_Size < 3 || N_Size > MAX_N_SIZE)
+	{
+		printf("N Size out of bounds\n");
+		abort();
+	}
+
 	int nextJobSize = min(MAX_JOB_SIZE, listSize);
 
 	if (nextJobSize > 0)
@@ -430,12 +428,136 @@ void primeTest(PrimeTestCxt* cxt, int N_Size, int listSize, const uint* M, uint*
 		}
 
 		DPRINTF("before execution\n");
+#define TEST(N) case N: fermat_test<N> << <numBlocks, blockSize >> >(cxt->m_mem_obj, cxt->mi_mem_obj, cxt->r_mem_obj, cxt->is_prime_mem_obj); break
 		switch (N_Size)
 		{
-		case 3: fermat_test<3> << <numBlocks, blockSize >> >(cxt->m_mem_obj, cxt->mi_mem_obj, cxt->r_mem_obj, cxt->is_prime_mem_obj); break;
-		case 20: fermat_test<20> << <numBlocks, blockSize >> >(cxt->m_mem_obj, cxt->mi_mem_obj, cxt->r_mem_obj, cxt->is_prime_mem_obj); break;
-		case 40: fermat_test<40> << <numBlocks, blockSize >> >(cxt->m_mem_obj, cxt->mi_mem_obj, cxt->r_mem_obj, cxt->is_prime_mem_obj); break;
-		case 53: fermat_test<53> <<<numBlocks, blockSize >>>(cxt->m_mem_obj, cxt->mi_mem_obj, cxt->r_mem_obj, cxt->is_prime_mem_obj); break;
+			TEST(3);
+			TEST(4);
+			TEST(5);
+			TEST(6);
+			TEST(7);
+			TEST(8);
+			TEST(9);
+			TEST(10);
+			TEST(11);
+			TEST(12);
+			TEST(13);
+			TEST(14);
+			TEST(15);
+			TEST(16);
+			TEST(17);
+			TEST(18);
+			TEST(19);
+			TEST(20);
+			TEST(21);
+			TEST(22);
+			TEST(23);
+			TEST(24);
+			TEST(25);
+			TEST(26);
+			TEST(27);
+			TEST(28);
+			TEST(29);
+			TEST(30);
+			TEST(31);
+			TEST(32);
+			TEST(33);
+			TEST(34);
+			TEST(35);
+			TEST(36);
+			TEST(37);
+			TEST(38);
+			TEST(39);
+			TEST(40);
+			TEST(41);
+			TEST(42);
+			TEST(43);
+			TEST(44);
+			TEST(45);
+			TEST(46);
+			TEST(47);
+			TEST(48);
+			TEST(49);
+			TEST(50);
+			TEST(51);
+			TEST(52);
+			TEST(53);
+			TEST(54);
+			TEST(55);
+			TEST(56);
+			TEST(57);
+			TEST(58);
+			TEST(59);
+			TEST(60);
+			TEST(61);
+			TEST(62);
+			TEST(63);
+			TEST(64);
+#if 0
+			TEST(65);
+			TEST(66);
+			TEST(67);
+			TEST(68);
+			TEST(69);
+			TEST(70);
+			TEST(71);
+			TEST(72);
+			TEST(73);
+			TEST(74);
+			TEST(75);
+			TEST(76);
+			TEST(77);
+			TEST(78);
+			TEST(79);
+			TEST(80);
+			TEST(81);
+			TEST(82);
+			TEST(83);
+			TEST(84);
+			TEST(85);
+			TEST(86);
+			TEST(87);
+			TEST(88);
+			TEST(89);
+			TEST(90);
+			TEST(91);
+			TEST(92);
+			TEST(93);
+			TEST(94);
+			TEST(95);
+			TEST(96);
+			TEST(97);
+			TEST(98);
+			TEST(99);
+			TEST(100);
+			TEST(101);
+			TEST(102);
+			TEST(103);
+			TEST(104);
+			TEST(105);
+			TEST(106);
+			TEST(107);
+			TEST(108);
+			TEST(109);
+			TEST(110);
+			TEST(111);
+			TEST(112);
+			TEST(113);
+			TEST(114);
+			TEST(115);
+			TEST(116);
+			TEST(117);
+			TEST(118);
+			TEST(119);
+			TEST(120);
+			TEST(121);
+			TEST(122);
+			TEST(123);
+			TEST(124);
+			TEST(125);
+			TEST(126);
+			TEST(127);
+#endif
 		default: abort();
 		}
 		
